8e9922ecb14e8dca8e37033fe65835bed3679de3
OrlandoFiallos <91402422+OrlandoFiallos@users.noreply.github.com>
2022-07-07 10:53:06 -0600

    Subida

---
+++ JS/scripts.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("menu");
+let menu_bar = document.getElementById("menu_bar");
+
+menu_bar.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+});
